90307e315aabc0271e21abae8d07c33835120494
mdarora <mdarora0001@gmail.com>
2023-12-18 23:10:42 -0500

    card styling

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,12 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Platform } from 'react-native'
 import React from 'react'
 
 export default function PokemonCard() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>PokemonCard</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    card:{
+        padding: 16,
+        margin: 16,
+        borderWidth:2,
+        borderColor: "black",
+        borderRadius: 16,
+        backgroundColor: "white",
+        ...Platform.select({
+            ios: {
+                shadowColor: "#333333",
+                shadowOffset: {width: 2, height: 2},
+                shadowOpacity: 0.3,
+                shadowRadius: 4
+            },
+            android: {
+                elevation: 5,
+            }
+        })
+    }
+})
